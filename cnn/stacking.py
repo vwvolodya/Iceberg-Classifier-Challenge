80e120a7167f13424e4b214e7bdf6cf15fd62e67
Holubets Volodymyr <vwvolodya@gmail.com>
2017-12-28 00:33:39 +0200

    added better stacking

--- cnn/stacking.py
+++ cnn/stacking.py
@@ -20,12 +20,13 @@ def evaluate(mdl, data, labels):
 
 p1 = "../data/train_predicted_inception.csv"
 p2 = "../data/train_predicted_lenet.csv"
-x = make_x(p1, p2)
+p3 = "../data/train_predicted_stats.csv"
+x = make_x(p1, p2, p3)
 data = pd.read_json("../data/orig/train.json")
 y = data["is_iceberg"].as_matrix()
 
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
 
 model = LogisticRegression(random_state=124, verbose=1, max_iter=150)
 model.fit(X_train, y_train)
@@ -39,12 +40,12 @@ model.fit(x, y)
 
 print(evaluate(model, x, y))
 
-target = make_x("../data/predicted_inception.csv", "../data/predicted_lenet.csv")
+target = make_x("../data/predicted_inception.csv", "../data/predicted_lenet.csv", "../data/predicted_stats.csv")
 target_id = pd.read_json("../data/orig/test.json").as_matrix(["id"])
 final_predictions = model.predict_proba(target)
 final_predictions = final_predictions[:, -1]
 
 final = np.column_stack((target_id, final_predictions))
 csv = pd.DataFrame(final, columns=["id", "is_iceberg"])
-csv.to_csv("../data/stacked.csv", float_format='%.6f', index=False)
+csv.to_csv("../data/stacked_3.csv", float_format='%.6f', index=False)
 print("Done")
